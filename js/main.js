7a1f9b2ab0ca50dc550d42fc5029b16f40f75920
Nawal-mahmoud <115593253+Nawal-mahmoud@users.noreply.github.com>
2022-10-12 01:51:13 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var typed = new Typed('.typing-name', {
+    strings: [' Nawal Mahmoud '],
+    typeSpeed:120,
+});
+var typed = new Typed('.typing-text', {
+    strings: [' Information technology engineer'],
+    typeSpeed:100,
+});
+var typed = new Typed('.typing-paragraph', {
+    strings: [' a graduate of El Shorouk Academy, majoring in Management Information Systems. now I specialize in information technology and networks, I have dealt with many programming languages for websites and applications and I have experience in information technology. I hope to join your group because I have the ability and skill to work under pressure and work hard in a team and I can make a change and add value to the employer as well as advance in my career , especially in the field of information technology.'],
+    typeSpeed:15,
+    
+});
+
